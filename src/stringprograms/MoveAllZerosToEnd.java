93bf5fab49d074fac49a941eb932ee3b11ef0e95
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-04-20 17:57:34 +0530

    "Java logic building - move all zeros to end"

---
+++ src/stringprograms/MoveAllZerosToEnd.java
@@ -0,0 +1,18 @@
+
+// Move all zeros to the end
+
+package stringprograms;
+
+public class MoveAllZerosToEnd {
+     public static void main(String[] args){
+        int input [] = {1,0,2,0,3};
+// The two-pointer technique is a method where we use two pointers to scan through an array or list, often from opposite ends or at different paces.
+// By moving these pointers toward each other or in a coordinated way, we can rearrange elements efficiently,
+// like grouping certain values, partitioning arrays, or finding pairs that meet a condition
+
+int current = 0;
+int nonzerovalue = 0;
+int temp = 0;
+
+     }
+}
