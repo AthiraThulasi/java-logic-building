2c2b915c281c90d0840844c58786c5fa802dc413
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-04-20 19:47:30 +0530

    updated move all zeros to the right

--- src/stringprograms/MoveAllZerosToEnd.java
+++ src/stringprograms/MoveAllZerosToEnd.java
@@ -4,15 +4,30 @@
 package stringprograms;
 
 public class MoveAllZerosToEnd {
-     public static void main(String[] args){
-        int input [] = {1,0,2,0,3};
+     public static void main(String[] args) {
+         int[] a = {1, 0, 2, 0, 3};
 // The two-pointer technique is a method where we use two pointers to scan through an array or list, often from opposite ends or at different paces.
 // By moving these pointers toward each other or in a coordinated way, we can rearrange elements efficiently,
 // like grouping certain values, partitioning arrays, or finding pairs that meet a condition
 
-int current = 0;
-int nonzerovalue = 0;
-int temp = 0;
+         int current = 0; // pointer for index// for traversing
+         int nonzerovalue = 0; // pointer for nonzerovalue
+         int temp = 0;
+
+         for (current = 0; current <= a.length - 1; current++) { // current traverse through the array
+             if (a[current] != 0) { // swap
+                 temp = a[nonzerovalue];
+                 a[nonzerovalue] = current;
+                 a[current] = temp;
+                 nonzerovalue++;
+             }
+         }
+
+         for (int output : a) {
+             System.out.print(output + " ");
+         }
+
 
      }
-}
+     }
+
