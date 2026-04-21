a226b8e4f7dcfbaaa22096dc4f9be039ff5055cc
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-04-21 09:43:06 +0530

    updated code - movenonzerotoend

--- src/stringprograms/MoveZerosToEndSeparateDigitsByCommaMaintainAscendingOrder.java
+++ src/stringprograms/MoveZerosToEndSeparateDigitsByCommaMaintainAscendingOrder.java
@@ -16,12 +16,14 @@ public class MoveZerosToEndSeparateDigitsByCommaMaintainAscendingOrder {
         int index = 0;
         for(int output : num){
             if (output!=0){
-                num[index] = output;
+                num[index] = output; // if num!= 0, overwrite with the same number
                 index++;
             }
-
+// The above logic does NOT remove old values at the back, It moves non-zero elements to the front.
+// After placing all non-zero elements at the front,fill the remaining positions with zeros
 for(int i = index; i<=num.length-1;i++){
-    num[i] = 0;
+    num[i] = 0; //Used a pointer to track the end of valid non-zero elements.
+               // Everything beyond that position is overwritten with zeros
 }
 
     Arrays.sort(num,0,index)    ;
