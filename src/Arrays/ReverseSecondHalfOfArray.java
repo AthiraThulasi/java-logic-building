893c6d3e1bce5e0c25665c4c9d064a9ca2054e4a
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-04-24 23:55:19 +0530

    Reverse SecondHalf Of Array added

---
+++ src/Arrays/ReverseSecondHalfOfArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ReverseSecondHalfOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int temp;
+        int mid = arr.length / 2;
+        int left = mid;
+        int right = arr.length - 1;
+
+        while (left < right) { // or right > left
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.print("Second Half reversal: ");
+        for (int number : arr) { // display the final reversed array
+            System.out.print(number + " ");
+        }
+    }
+}
+
+
