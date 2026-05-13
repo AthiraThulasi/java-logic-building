23bc662b2c7060976ff3ba5347cea721ca540406
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-13 22:11:47 +0530

    added length of last word in string

---
+++ src/StringManipulation/PrintTheLengthOfLastWordInString.java
@@ -0,0 +1,23 @@
+package StringManipulation;
+
+public class PrintTheLengthOfLastWordInString {
+
+        public static void main(String[] args) {
+            String s = "        Hello world     moon        ";
+            System.out.println(s);
+            System.out.println(s);
+            int count = 0;
+            char inputArray[] = s.toCharArray();
+            for (int i = inputArray.length - 1; i >= 0; i--) {
+                if (inputArray[i] != ' ') {
+                    count = count + 1;
+                } else {
+                    if (count > 0) {
+                        System.out.println(count);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
