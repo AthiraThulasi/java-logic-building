633f1031592845fb328721ceb62b3c4c04298987
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-18 13:52:24 +0530

    string program updation

---
+++ src/StringManipulation/RemoveDuplicatesFromString.java
@@ -0,0 +1,27 @@
+package StringManipulation;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesFromString {
+
+    public static void main(String[] args){
+
+        String name = "aathiraa";
+
+        Set<Character> set= new LinkedHashSet<>();
+        for(int i =0; i<name.length();i++) { // Forward loop	i < length
+
+            set.add(name.charAt(i));
+        }
+
+            for(Character ch : set)
+
+                System.out.print(ch);
+
+        }
+
+
+
+    }
+
--- src/StringManipulation/ReverseAString.java
+++ src/StringManipulation/ReverseAString.java
@@ -6,7 +6,7 @@ public class ReverseAString {
 
     public static void main(String[] args) {
         String name = "Athira";
-for(int i = name.length()-1;i>0; i--){
+for(int i = name.length()-1;i >= 0; i--){ // Reverse loop -	length - 1
     // length() is a method in String used to get the number of characters in the string.
     System.out.print(name.charAt(i));
 }
