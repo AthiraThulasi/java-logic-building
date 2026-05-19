3024cee3cce1f8909b407160062780ebea321dbe
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-19 10:29:05 +0530

    updated the comments in code

--- src/StringManipulation/CountOccurrenceOfEachCharacter.java
+++ src/StringManipulation/CountOccurrenceOfEachCharacter.java
@@ -5,20 +5,25 @@ import java.util.Map;
 
 public class CountOccurrenceOfEachCharacter {
     public static void main(String[] args){
+
         Map<Character,Integer> map = new HashMap<>();
+        // // Map stores character as key and count as value
 
         String name = "Aathiraaa";
+
         char [] nameoutput = name.toCharArray();
-        for(char ch : nameoutput)
+        //  Converts String into char array >> ['A','a','t','h','i','r','a','a','a']
+        for(char ch : nameoutput) // Enhanced for loop to get each character one by one
         {
-        if(map.containsKey(ch))
+        if(map.containsKey(ch)) // Checks whether character already exists in map
             {
-            map.put(ch,map.get(ch) +1);
-            }
+            map.put(ch,map.get(ch) +1); // map.get(ch)+1 >> Gives the current count/value of the character
+            }      // Gets existing count and increases by 1
 
             else
             {
-                map.put(ch,1);
+                map.put(ch,1); // First time character appears, so count is set to 1
+                // First time character comes → store count as 1
             }
         }
         System.out.println(map);
