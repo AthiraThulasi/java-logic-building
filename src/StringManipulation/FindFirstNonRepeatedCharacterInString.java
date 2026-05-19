40165fe58a8ec079f6317441c8ad6a99502302bc
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-19 12:23:13 +0530

    added FindFirstNonRepeatedCharacterInString

--- src/StringManipulation/FindFirstNonRepeatedCharacterInString.java
+++ src/StringManipulation/FindFirstNonRepeatedCharacterInString.java
@@ -1,5 +1,32 @@
 package StringManipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindFirstNonRepeatedCharacterInString {
 
+    public static void main(String[] args) {
+        Map<Character, Integer> map = new HashMap<>();
+        String name = "AAaathiraaa";
+        char[] namechar = name.toCharArray();
+        for (char ch : namechar) {
+            if (map.containsKey(ch)) {
+
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+
+        }
+
+        for (char ch : namechar) {
+            {
+                if (map.get(ch) == 1) {
+                    System.out.println("First non-repeated character is: " + ch);
+                    break;
+                }
+            }
+        }
+    }
 }
+
