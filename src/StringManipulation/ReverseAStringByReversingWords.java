14aedacd93c62edb95051caef4c91ab378a89dac
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-20 19:39:18 +0530

    code updation

--- src/StringManipulation/ReverseAStringByReversingWords.java
+++ src/StringManipulation/ReverseAStringByReversingWords.java
@@ -4,13 +4,19 @@ public class ReverseAStringByReversingWords {
 
     public static void main(String[] args) {
         String name = "Athira works at Infosys";
-       String [] words = name.split(" "); // split() returns a String Array
-       for(int i = words.length-1;i >=0; i-- ){ // Outer loop → reverses the word order
-           // length is an array property used to find the size of an array.
-           String reversedWord = "";
-           for (int j = words[i].length() - 1; j >= 0; j--) { // Inner loop → reverses the characters inside each word
-               reversedWord = reversedWord + words[i].charAt(j);
-           } System.out.print(reversedWord + " "); }
+        String[] words = name.split(" "); // split() returns a String Array >> ["Athira", "works", "at", "Infosys"]
+
+        // Outer loop → reverses the word order
+        for (int i = words.length - 1; i >= 0; i--) {
+            // length is an array property used to find the size of an array.
+            String reversedWord = "";
+
+            // Inner loop → reverses the characters inside each word
+            for (int j = words[i].length() - 1; j >= 0; j--) {
+                reversedWord = reversedWord + words[i].charAt(j);
+            }
+            System.out.print(reversedWord + " ");
         }
     }
+}
 // OUTPUT - sysofnI ta skrow arihtA
