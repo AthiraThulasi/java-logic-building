374ff808b2b29ac5a4920d965eba20ba40548ef4
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-20 20:03:07 +0530

    logic updated

--- src/StringManipulation/ReverseAStringByReversingWords.java
+++ src/StringManipulation/ReverseAStringByReversingWords.java
@@ -9,7 +9,8 @@ public class ReverseAStringByReversingWords {
         // Outer loop → reverses the word order
         for (int i = words.length - 1; i >= 0; i--) {
             // length is an array property used to find the size of an array.
-            String reversedWord = "";
+
+            String reversedWord = "";//Need a blank variable to start building the reversed word character by character.
 
             // Inner loop → reverses the characters inside each word
             for (int j = words[i].length() - 1; j >= 0; j--) {
@@ -19,4 +20,15 @@ public class ReverseAStringByReversingWords {
         }
     }
 }
+
+//LOGIC
+//Outer loop
+//   ↓
+//Picks one word at a time from last to first
+//   ↓
+//Inner loop
+//   ↓
+//Reverses characters of that word
+
+
 // OUTPUT - sysofnI ta skrow arihtA
