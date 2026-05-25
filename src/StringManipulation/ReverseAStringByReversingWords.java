1ea10649f5b8befc31f9837a8fab3c51b743c850
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-25 14:55:38 +0530

    code updated

--- src/StringManipulation/ReverseAStringByReversingWords.java
+++ src/StringManipulation/ReverseAStringByReversingWords.java
@@ -6,7 +6,7 @@ public class ReverseAStringByReversingWords {
         String name = "Athira works at Infosys";
         String[] words = name.split(" "); // split() returns a String Array >> ["Athira", "works", "at", "Infosys"]
 
-        // Outer loop → reverses the word order
+        // Outer loop  → picks each word from end
         for (int i = words.length - 1; i >= 0; i--) {
             // length is an array property used to find the size of an array.
 
@@ -14,7 +14,7 @@ public class ReverseAStringByReversingWords {
 
             // Inner loop → reverses the characters inside each word
             for (int j = words[i].length() - 1; j >= 0; j--) { // words[i] is a one single element from array ie String, so use length()
-                reversedWord = reversedWord + words[i].charAt(j);
+                reversedWord = reversedWord + words[i].charAt(j);//charAt(j) → fetches each character one by one from the current word
             }
             System.out.print(reversedWord + " ");
         }
@@ -22,13 +22,12 @@ public class ReverseAStringByReversingWords {
 }
 
 //LOGIC
-//Outer loop
-//   ↓
-//Picks one word at a time from last to first
-//   ↓
-//Inner loop
-//   ↓
-//Reverses characters of that word
-
-
+/*
+split(" ") converts the sentence into a String array of words.
+Outer loop starts from the last index and picks words in reverse order.
+words[i] gives one word at a time (example: "Infosys").
+Inner loop uses words[i].length() - 1 to start from the last character of the current word.
+charAt(j) fetches each character one by one from the current word in reverse direction.
+reversedWord = reversedWord + words[i].charAt(j) keeps building the reversed word character by character.
 // OUTPUT - sysofnI ta skrow arihtA
+*/
