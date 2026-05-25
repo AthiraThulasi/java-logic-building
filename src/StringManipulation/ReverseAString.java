ded1e7cfea02b9b51103bd2cd4c3f21a862eb69e
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-25 07:41:42 +0530

    updated ReverseStringWithoutReversingWords

--- src/StringManipulation/ReverseAString.java
+++ src/StringManipulation/ReverseAString.java
@@ -6,9 +6,10 @@ public class ReverseAString {
 
     public static void main(String[] args) {
         String name = "Athira";
-for(int i = name.length()-1;i >= 0; i--){ // Reverse loop -	length - 1
+for(int i = name.length()-1;i >= 0; i--){ //for loop - changes index values one by one
     // length() is a method in String used to get the number of characters.
-    System.out.print(name.charAt(i));
+    System.out.print(name.charAt(i));//charAt(i) → goes to that index and gets the character
+
     // charAt() is memory efficient because it accesses characters directly from the String without creating a new array.
     // toCharArray() creates an extra character array in memory.
 }
--- src/StringManipulation/ReverseStringWithoutReversingWords.java
+++ src/StringManipulation/ReverseStringWithoutReversingWords.java
@@ -5,8 +5,8 @@ public class ReverseStringWithoutReversingWords {
     public static void main(String[] args) {
         String s = "I work at Infosys";
 
-        String[] words = s.split(" "); // Split the string wherever there is a space.
-        // split() converts a single String into a String[] based on delimiter (" ")
+        String[] words = s.split(" ");  // output - ["I","work","at","Infosys"] - String Array
+        // Split the string wherever there is a space.
 
         for (int i = words.length - 1; i >= 0; i--) {
             System.out.print(words[i] + " ");
