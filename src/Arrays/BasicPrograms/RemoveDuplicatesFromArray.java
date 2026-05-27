7f7988353d32097461ad4dfb1c188ec7d0fda221
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-27 13:14:26 +0530

    RemoveDuplicatesFromArray

--- src/Arrays/BasicPrograms/RemoveDuplicatesFromArray.java
+++ src/Arrays/BasicPrograms/RemoveDuplicatesFromArray.java
@@ -1,8 +1,19 @@
 package Arrays.BasicPrograms;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class RemoveDuplicatesFromArray {
 
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,20,10};
-    }
-}
+
+        Set<Integer> set = new LinkedHashSet<>();
+        for(int num :arr) {
+            set.add(num);
+        }
+
+        System.out.println("Without Duplicates: " + set);
+            }
+        }
+
