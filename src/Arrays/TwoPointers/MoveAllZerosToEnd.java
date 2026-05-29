a53297327481e402a19a78ce6400915181a7f21e
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-05-29 12:04:17 +0530

    updated code

--- src/Arrays/TwoPointers/MoveAllZerosToEnd.java
+++ src/Arrays/TwoPointers/MoveAllZerosToEnd.java
@@ -1,5 +1,7 @@
 package Arrays.TwoPointers;
 
+import javax.security.auth.login.LoginContext;
+
 public class MoveAllZerosToEnd {
 
     public static void main(String[] args) {
@@ -8,7 +10,8 @@ public class MoveAllZerosToEnd {
 
             int putNonZeroValuesHere = 0;
             int temp; // swapping!!
-
+// currentScans every element one by one
+// NonZeroValuesHereMarks where the next non-zero should land
             for (int current = 0; current < a.length; current++) {
                 if (a[current] != 0) {
                     // Swap
@@ -24,6 +27,17 @@ public class MoveAllZerosToEnd {
             }
         }
     }
+
+
+//Logic
+
+//This program has Two Pointers because:
+
+//Two variables both holding indexes - current & putNonZeroValuesHere
+//Both pointing at the same array - But moving at different speeds
+// One pointer is fast → scans everything
+// One pointer is slow → only moves when a condition is met
+// Every time a non-zero and a zero swap places:Non-zero goes left  and Zero goes right
 
 
 
