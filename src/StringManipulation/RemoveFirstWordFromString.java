d85c46caf955a470a274aa536cacdf65276a85a3
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-03 11:09:50 +0530

    updated code

--- src/StringManipulation/RemoveFirstWordFromString.java
+++ src/StringManipulation/RemoveFirstWordFromString.java
@@ -4,12 +4,13 @@ public class RemoveFirstWordFromString {
 
     public static void main(String[] args) {
 
-        String s = "Athira works at Infosys";
+        String s = "Athira works at Capgemini";
 
         String[] StrOutput = s.split(" "); // Output is a String Array = {"Athira","works","at","Infosys"};
+        // split(" ") removes the spaces and stores only the words
 
         for (int i = 1; i < StrOutput.length; i++) { // length → property for array - is used to find the length
-            System.out.println(StrOutput[i]);
+            System.out.print(StrOutput[i] + " ");
 
 
         }
