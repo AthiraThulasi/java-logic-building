1968652f098548548d8482b68faa06f02340cf8d
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-03 10:57:39 +0530

    Remove first word using substring

---
+++ src/StringManipulation/RemoveFirstWordFromStringUsingSubString.java
@@ -0,0 +1,13 @@
+package StringManipulation;
+
+public class RemoveFirstWordFromStringUsingSubString {
+
+        public static void main(String[] args) {
+            String s = "Athira works at Capgemini";
+            String result = s.substring(s.indexOf(" ")+1);// Start the substring from the position of the first space
+            System.out.println(result);                      // +1 skips the first space
+// The indexOf(" ") solution works when words are separated by spaces.
+        }
+    }
+
+
